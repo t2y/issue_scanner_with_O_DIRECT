491c0a75e6c2d0a9ec9d43900484e65a48e55be9
Tetsuya Morimoto <tetsuya.morimoto@gmail.com>
2017-07-31 22:20:06 +0900

    fix to be able to read with O_DIRECT by making buffer 512 KiB

--- main.go
+++ main.go
@@ -11,21 +11,24 @@ var direct bool
 
 func Read(path string) {
 	var (
-		f   *os.File
-		err error
+		f       *os.File
+		err     error
+		bufSize int
 	)
 
 	if direct {
 		f, err = os.OpenFile(path, os.O_RDONLY|syscall.O_DIRECT, 0)
+		bufSize = 524288
 	} else {
 		f, err = os.OpenFile(path, os.O_RDONLY, 0)
+		bufSize = 100
 	}
 	if err != nil {
 		log.Println("error: ", err)
 	}
 	defer f.Close()
 
-	data := make([]byte, 100)
+	data := make([]byte, bufSize)
 	for {
 		n, err := f.Read(data)
 		log.Print(n, ", ", string(data))
